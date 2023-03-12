e3aa3448a6fd66716f390bf93023ccdb5eb95d40
Manas171202 <114315775+Manas171202@users.noreply.github.com>
2023-03-12 23:19:19 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmNwjTz9YFblRRyE99eaKTPrHF95AEo64",
+    authDomain: "stock-market-cbade.firebaseapp.com",
+    projectId: "stock-market-cbade",
+    storageBucket: "stock-market-cbade.appspot.com",
+    messagingSenderId: "765522212217",
+    appId: "1:765522212217:web:6693f4d34c049f941a2ba8",
+    measurementId: "G-ZFNVE7CMP9"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+  
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
